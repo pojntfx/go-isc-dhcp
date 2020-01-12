8a134534b1e50002ac4e7b20be5bd7d4a3b48a0a
Felix Pojtinger <felix@pojtinger.com>
2020-01-12 15:16:52 +0100

    feat: Add `get` command for `dhcpdctl` (#5)

---
+++ cmd/dhcpdctl/cmd/root.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"gitlab.com/bloom42/libs/rz-go"
+	"gitlab.com/bloom42/libs/rz-go/log"
+	"strings"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "dhcpdctl",
+	Short: "dhcpdctl manages dhcpdd, the ISC DHCP server management daemon",
+	Long: `dhcpdctl manages dhcpdd, the ISC DHCP server management daemon.
+
+Find more information at:
+https://pojntfx.github.io/godhcpd/`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		viper.SetEnvPrefix("dhcpd")
+		viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_", ".", "_"))
+	},
+}
+
+// Execute starts the root command.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal("Could not start root command", rz.Err(err))
+	}
+}
---
+++ cmd/dhcpdctl/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/pojntfx/godhcpd/cmd/dhcpdctl/cmd"
+
+func main() {
+	cmd.Execute()
+}
