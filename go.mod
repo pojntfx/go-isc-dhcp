10bdc4fac1c1f2992d66931ff03b1b64b002e36d
Felix Pojtinger <felix@pojtinger.com>
2020-01-12 02:22:52 +0100

    feat: Add initial gRPC service and server for ISC DHCP server manager

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/pojntfx/godhcpd
 go 1.13
 
 require (
+	github.com/golang/protobuf v1.3.2
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/rakyll/statik v0.1.6
 	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/cobra v0.0.5
+	gitlab.com/bloom42/libs/rz-go v1.3.0
+	google.golang.org/grpc v1.26.0
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- pkg/svc/dhcpd.go
+++ pkg/svc/dhcpd.go
@@ -1,17 +1,88 @@
 package svc
 
-//go:generate sh -c "rm -rf dhcp statik; git clone https://gitlab.isc.org/isc-projects/dhcp.git; cd dhcp; ./configure; make; mkdir -p dist; cp server/dhcpd dist; cp client/dhclient dist; statik -src dhcp/dist"
+//go:generate sh -c "mkdir -p ../proto/generated && protoc --go_out=paths=source_relative,plugins=grpc:../proto/generated -I=../proto ../proto/*.proto"
+//go:generate sh -c "rm -rf dhcp statik; git clone https://gitlab.isc.org/isc-projects/dhcp.git; cd dhcp; ./configure; make; mkdir -p dist; cp server/dhcpd dist; cp client/dhclient dist; cd ../; statik -src dhcp/dist"
 
 import (
-	_ "github.com/pojntfx/godhcpd/pkg/svc/statik"
+	"context"
+	godhcpd "github.com/pojntfx/godhcpd/pkg/proto/generated"
+	_ "github.com/pojntfx/godhcpd/pkg/svc/statik" // Embedded ISC DHCP server binary
+	"github.com/pojntfx/godhcpd/pkg/workers"
 	"github.com/rakyll/statik/fs"
+	uuid "github.com/satori/go.uuid"
+	"gitlab.com/bloom42/libs/rz-go/log"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"os"
+	"path/filepath"
 )
 
+// DHCPDManager manages DHCP servers.
 type DHCPDManager struct {
-	BinaryDir string
+	godhcpd.UnimplementedDHCPDManagerServer
+	BinaryDir     string
+	StateDir      string
+	DHCPDsManaged map[string]*workers.DHCPD
 }
 
+// Create creates a DHCP server.
+func (m *DHCPDManager) Create(_ context.Context, args *godhcpd.DHCPDManagerCreateArgs) (*godhcpd.DHCPDManagerCreateReply, error) {
+	id := uuid.NewV4().String()
+
+	subnets := args.GetSubnets()
+	var subnetsForWorker []workers.Subnet
+	for _, subnet := range subnets {
+		subnetsForWorker = append(subnetsForWorker, workers.Subnet{
+			Network: subnet.GetNetwork(),
+			Netmask: subnet.GetNetmask(),
+			Range: workers.Range{
+				Start: subnet.GetRange().GetStart(),
+				End:   subnet.GetRange().GetEnd(),
+			},
+		})
+	}
+
+	dhcpd := workers.DHCPD{
+		Subnets:   subnetsForWorker,
+		Device:    args.GetDevice(),
+		ID:        id,
+		BinaryDir: m.BinaryDir,
+		StateDir:  filepath.Join(m.StateDir, id),
+	}
+
+	if err := dhcpd.Configure(); err != nil {
+		log.Error(err.Error())
+
+		return nil, status.Errorf(codes.Unknown, err.Error())
+	}
+
+	if err := dhcpd.Start(); err != nil {
+		log.Error(err.Error())
+	}
+
+	go func(dhcpd *workers.DHCPD) {
+		log.Info("Starting dhcp server")
+
+		// Keep the dhcp server running
+		for {
+			_ = dhcpd.Wait()
+
+			log.Info("Restarting dhcp server")
+
+			if err := dhcpd.Start(); err != nil {
+				log.Error(err.Error())
+			}
+		}
+	}(&dhcpd)
+
+	m.DHCPDsManaged[id] = &dhcpd
+
+	return &godhcpd.DHCPDManagerCreateReply{
+		Id: id,
+	}, nil
+}
+
+// Extract extracts the ISC DHCP server binary.
 func (m *DHCPDManager) Extract() error {
 	statikFS, err := fs.New()
 	if err != nil {
