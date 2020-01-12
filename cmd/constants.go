abc36a9fbb6707bb33d643feb4c302d377eea87c
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-01-12 11:52:48 +0100

    feat: Add Cobra CLI for dhpcdd

---
+++ cmd/constants.go
@@ -0,0 +1,10 @@
+package cmd
+
+const (
+	DHCPDDHostPortDefault = "localhost:1240" // DHCPDDHostPortDefault is the default Host:port that of `dhcpdd`.
+)
+
+const (
+	CouldNotBindFlagsErrorMessage        = "Could not bind flags"         // CouldNotBindFlagsErrorMessage is the error message to throw if binding the flags has failed.
+	CouldNotStartRootCommandErrorMessage = "Could not start root command" // CouldNotStartRootCommandErrorMessage is the error message to throw if starting the root command has failed.
+)
