061480aba7ede331a627494d97abe315eee68b5f
Felix Pojtinger <felix@pojtinger.com>
2020-01-12 11:52:48 +0100

    feat: Add Cobra CLI for dhpcdd

--- cmd/dhcpdd/main.go
+++ cmd/dhcpdd/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
+	constants "github.com/pojntfx/godhcpd/cmd"
 	godhcpd "github.com/pojntfx/godhcpd/pkg/proto/generated"
 	"github.com/pojntfx/godhcpd/pkg/svc"
 	"github.com/pojntfx/godhcpd/pkg/workers"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"gitlab.com/bloom42/libs/rz-go"
 	"gitlab.com/bloom42/libs/rz-go/log"
 	"google.golang.org/grpc"
@@ -13,66 +15,117 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
+	"strings"
 	"syscall"
 )
 
-func main() {
-	binaryDir := filepath.Join(os.TempDir(), "dhcpd")
+const (
+	keyPrefix         = "dhcpdd."
+	configFileDefault = ""
+	configFileKey     = keyPrefix + "configFile"
+	listenHostPortKey = keyPrefix + "listenHostPort"
+)
 
-	listener, err := net.Listen("tcp", "localhost:30001")
-	if err != nil {
-		fmt.Println(err)
-	}
+var rootCmd = &cobra.Command{
+	Use:   "dhcpdd",
+	Short: "dhcpdd is the ISC DHCP server management daemon",
+	Long: `dhcpdd is the ISC DHCP server management daemon.
 
-	server := grpc.NewServer()
-	reflection.Register(server)
+Find more information at:
+https://pojntfx.github.io/godhcpd/`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		viper.SetEnvPrefix("dhcpdd")
+		viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_", ".", "_"))
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if !(viper.GetString(configFileKey) == configFileDefault) {
+			viper.SetConfigFile(viper.GetString(configFileKey))
 
-	DHCPDService := svc.DHCPDManager{
-		BinaryDir:     binaryDir,
-		StateDir:      filepath.Join(os.TempDir(), "godhcpd", "dhcpd"),
-		DHCPDsManaged: make(map[string]*workers.DHCPD),
-	}
+			if err := viper.ReadInConfig(); err != nil {
+				return err
+			}
+		}
+		binaryDir := filepath.Join(os.TempDir(), "dhcpd")
 
-	godhcpd.RegisterDHCPDManagerServer(server, &DHCPDService)
+		listener, err := net.Listen("tcp", viper.GetString(listenHostPortKey))
+		if err != nil {
+			return err
+		}
 
-	interrupt := make(chan os.Signal, 2)
-	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-interrupt
+		server := grpc.NewServer()
+		reflection.Register(server)
 
-		// Allow manually killing the process
+		DHCPDService := svc.DHCPDManager{
+			BinaryDir:     binaryDir,
+			StateDir:      filepath.Join(os.TempDir(), "godhcpd", "dhcpd"),
+			DHCPDsManaged: make(map[string]*workers.DHCPD),
+		}
+
+		godhcpd.RegisterDHCPDManagerServer(server, &DHCPDService)
+
+		interrupt := make(chan os.Signal, 2)
+		signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
 		go func() {
 			<-interrupt
 
-			os.Exit(1)
+			// Allow manually killing the process
+			go func() {
+				<-interrupt
+
+				os.Exit(1)
+			}()
+
+			log.Info("Gracefully stopping server (this might take a few seconds)")
+
+			msg := "Could not stop dhcp server"
+
+			for _, DHCPD := range DHCPDService.DHCPDsManaged {
+				if err := DHCPD.Stop(); err != nil {
+					log.Fatal(msg, rz.Err(err))
+				}
+			}
+
+			for _, DHCPD := range DHCPDService.DHCPDsManaged {
+				if err := DHCPD.Wait(); err != nil {
+					log.Fatal(msg, rz.Err(err))
+				}
+			}
+
+			server.GracefulStop()
 		}()
 
-		log.Info("Gracefully stopping server (this might take a few seconds)")
-
-		msg := "Could not stop dhcp server"
-
-		for _, DHCPD := range DHCPDService.DHCPDsManaged {
-			if err := DHCPD.Stop(); err != nil {
-				log.Fatal(msg, rz.Err(err))
-			}
+		if err := DHCPDService.Extract(); err != nil {
+			return err
 		}
 
-		for _, DHCPD := range DHCPDService.DHCPDsManaged {
-			if err := DHCPD.Wait(); err != nil {
-				log.Fatal(msg, rz.Err(err))
-			}
+		log.Info("Starting server")
+
+		if err := server.Serve(listener); err != nil {
+			return err
 		}
 
-		server.GracefulStop()
-	}()
+		return nil
+	},
+}
 
-	if err := DHCPDService.Extract(); err != nil {
-		fmt.Println(err)
+func init() {
+	var (
+		configFileFlag string
+		hostPortFlag   string
+	)
+
+	rootCmd.PersistentFlags().StringVarP(&configFileFlag, configFileKey, "f", configFileDefault, "Configuration file to use.")
+	rootCmd.PersistentFlags().StringVarP(&hostPortFlag, listenHostPortKey, "l", constants.DHCPDDHostPortDefault, "TCP listen host:port.")
+
+	if err := viper.BindPFlags(rootCmd.PersistentFlags()); err != nil {
+		log.Fatal(constants.CouldNotBindFlagsErrorMessage, rz.Err(err))
 	}
 
-	log.Info("Starting server")
+	viper.AutomaticEnv()
+}
 
-	if err := server.Serve(listener); err != nil {
-		fmt.Println(err)
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(constants.CouldNotStartRootCommandErrorMessage, rz.Err(err))
 	}
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/rakyll/statik v0.1.6
 	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.3.2
 	gitlab.com/bloom42/libs/rz-go v1.3.0
 	google.golang.org/grpc v1.26.0
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
