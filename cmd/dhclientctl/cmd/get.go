d125b96be99b189890a14465d62712205144d103
Felix Pojtinger <felix@pojtinger.com>
2020-01-27 17:04:30 +0100

    fix: Use common pointer for common flags for `dhclientctl`

--- cmd/dhclientctl/cmd/get.go
+++ cmd/dhclientctl/cmd/get.go
@@ -71,10 +71,6 @@ var getCmd = &cobra.Command{
 }
 
 func init() {
-	var (
-		serverHostPortFlag string
-	)
-
 	getCmd.PersistentFlags().StringVarP(&serverHostPortFlag, serverHostPortKey, "s", constants.DHClientDHostPortDefault, constants.HostPortDocs)
 
 	if err := viper.BindPFlags(getCmd.PersistentFlags()); err != nil {
