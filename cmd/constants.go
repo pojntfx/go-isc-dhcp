14c10e581df737e76895531fd9390a4be68760ad
Felix Pojtinger <felix@pojtinger.com>
2020-03-18 17:32:51 +0100

    refactor: Standardize ports and ingress hosts

--- cmd/constants.go
+++ cmd/constants.go
@@ -1,8 +1,8 @@
 package cmd
 
 const (
-	DHCPDDHostPortDefault    = "localhost:1240"                  // DHCPDDHostPortDefault is the default Host:port of `dhcpdd`.
-	DHClientDHostPortDefault = "localhost:1241"                  // DHClientDHostPortDefault is the default Host:port of `dhclient`.
+	DHCPDDHostPortDefault    = "localhost:1020"                  // DHCPDDHostPortDefault is the default Host:port of `dhcpdd`.
+	DHClientDHostPortDefault = "localhost:1030"                  // DHClientDHostPortDefault is the default Host:port of `dhclient`.
 	HostPortDocs             = "Host:port of the server to use." // HostPortDocs is the documentation for the host:port flag.
 	ConfigurationFileDocs    = "Configuration file to use."      // ConfigurationFileDocs is the documentation for the configuration file flag.)
 )
